0db2ee4452f2cf210578d819e067cc51e226bc76
Shai <142772851+shai360hai@users.noreply.github.com>
2025-08-07 13:50:20 +0300

    Update PastryContactCard.tsx

--- src/components/PastryContactCard.tsx
+++ src/components/PastryContactCard.tsx
@@ -3,34 +3,30 @@ import "./PastryContactCard.css";
 
 const PastryContactCard = () => {
   return (
-    <div className="pastry-container">
-      <img src="/images/logo.jpeg" alt="logo" className="logo" />
-      <img src="/images/mixer.png" alt="מיקסר" className="mixer" />
-
-      <div className="contact">
-        <div className="icons">
-          <a href="tel:0506897798">
-            <img
-              src="https://img.icons8.com/ios-filled/50/000000/phone.png"
-              alt="Call"
-            />
+    <div className="pastry-card">
+      <div className="pastry-card__header">
+        <img src="/images/logo.jpeg" alt="Pastry Magic Logo" className="pastry-card__logo" />
+        <img src="/images/mixer.png" alt="Mixer illustration" className="pastry-card__mixer" />
+      </div>
+      <div className="pastry-card__contact">
+        <div className="pastry-card__contact-item">
+          <a href="tel:0506897798" className="pastry-card__icon-link" title="Call">
+            <img src="https://img.icons8.com/ios-filled/50/000000/phone.png" alt="Call Phone" />
           </a>
-          <a href="https://wa.me/972506897798" target="_blank" rel="noreferrer">
+          <a href="https://wa.me/972506897798" target="_blank" rel="noreferrer" className="pastry-card__contact-number">
             050-6897798
           </a>
         </div>
-
-        <div className="instagram">
+        <div className="pastry-card__contact-item">
           <a
             href="https://www.instagram.com/haisasonker"
             target="_blank"
             rel="noreferrer"
+            className="pastry-card__icon-link"
+            title="Instagram"
           >
-            <img
-              src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png"
-              alt="Instagram"
-            />
-            @haisasonker
+            <img src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png" alt="Instagram profile" />
+            <span className="pastry-card__instagram">@haisasonker</span>
           </a>
         </div>
       </div>
