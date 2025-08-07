0a07c92a49a8c86a7eeaf6c6a2eade15bab7fa7f
Shai <142772851+shai360hai@users.noreply.github.com>
2025-08-07 13:31:51 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,12 @@
 import InvoiceGenerator from "@/components/InvoiceGenerator";
+import PastryContactCard from "@/components/PastryContactCard";
+
 
 const Index = () => {
+      <div>
+      <PastryContactCard />
+      {/* תוכן נוסף אם קיים */}
+    </div>
   return <InvoiceGenerator />;
 };
 
